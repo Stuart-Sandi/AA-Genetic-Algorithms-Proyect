66ef44b599e9d83e3c6c67a5359c445aaeee001e
Brayan Marín Quirós <brayanmq11288@gmail.com>
2021-05-26 21:32:49 -0600

    Fractal changes

--- Genetic_Algorithms/Model/Fractal.py
+++ Genetic_Algorithms/Model/Fractal.py
@@ -14,20 +14,20 @@ class Tree:
         pygame.display.set_caption("Fractal Tree")
         self.screen = pygame.display.get_surface()
 
-        self.drawTree(300, 550, -90, 10)
+        self.drawTree(300, 550, -90, 9, 2, 20.0, 10.0)
         pygame.display.flip()
         while True:
             self.input(pygame.event.wait())
 
-    def drawTree(self, x1, y1, angle, depth):
-        fork_angle = 20
-        base_len = 10.0
+    def drawTree(self, x1, y1, angle, depth, branch_thickness, fork_angle, base_len):
         if depth > 0:
             x2 = x1 + int(math.cos(math.radians(angle)) * depth * base_len)
             y2 = y1 + int(math.sin(math.radians(angle)) * depth * base_len)
-            pygame.draw.line(self.screen, (255, 255, 255), (x1, y1), (x2, y2), 2)
-            self.drawTree(x2, y2, angle - fork_angle, depth - 1)
-            self.drawTree(x2, y2, angle + fork_angle, depth - 1)
+
+            pygame.draw.line(self.screen, (255, 255, 255), (x1, y1), (x2, y2), branch_thickness)
+
+            self.drawTree(x2, y2, angle - fork_angle, depth - 1, branch_thickness, fork_angle, base_len)
+            self.drawTree(x2, y2, angle + fork_angle, depth - 1, branch_thickness, fork_angle, base_len)
 
     def input(self, event):
         if event.type == pygame.QUIT:
