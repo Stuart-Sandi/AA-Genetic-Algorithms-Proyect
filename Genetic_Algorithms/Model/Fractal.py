f229bfd49308821accaf3fdf918eac2573bdcff8
Stuart-Sandi <66484767+Stuart-Sandi@users.noreply.github.com>
2021-05-29 11:34:18 -0600

    Grosor de las ramas

--- Genetic_Algorithms/Model/Fractal.py
+++ Genetic_Algorithms/Model/Fractal.py
@@ -4,7 +4,6 @@ import random
 
 
 class Tree:
-
     window = None
     screen = None
 
@@ -16,40 +15,53 @@ class Tree:
         self.screen = pygame.display.get_surface()
 
         x1 = 300
-        x2 = 550
+        y1 = 550
         angle = -90
-        depth = 5
+        depth = 7
+        grosor = 8
         branch_thickness = (1, 2)
         fork_angle = (20.0, 30.0)
         branch_quantity = (1, 3)
         base_len = (10.0, 15.0)
 
-        self.drawTree(x1, x2, angle, depth, branch_thickness, fork_angle, base_len, branch_quantity)
+        self.drawTree(x1, y1, angle, depth, branch_thickness, fork_angle, base_len, branch_quantity, grosor)
         pygame.display.flip()
         while True:
             self.input(pygame.event.wait())
 
-    def drawTree(self, x1, y1, angle, depth, branch_thickness, fork_angle, base_len, branch_quantity):
+    def drawTree(self, x1, y1, angle, depth, branch_thickness, fork_angle, base_len, branch_quantity, grosor):
         if depth > 0:
             base_len_random = random.randint(base_len[0], base_len[1])
             x2 = x1 + int(math.cos(math.radians(angle)) * depth * base_len_random)
             y2 = y1 + int(math.sin(math.radians(angle)) * depth * base_len_random)
 
-            branch_thickness_random = random.randint(branch_thickness[0], branch_thickness[1])
-            pygame.draw.line(self.screen, (255, 255, 255), (x1, y1), (x2, y2), branch_thickness_random)
+            # Grosor del arbol
+            grosor2 = grosor
+            if grosor2 < 0:
+                grosor2 = 0
+
+            while grosor2 != -1:
+                branch_thickness_random = random.randint(branch_thickness[0], branch_thickness[1])
+                pygame.draw.line(self.screen, (255, 255, 255), (x1 - grosor2, y1), (x2 - grosor2, y2),
+                                 branch_thickness_random)
+                pygame.draw.line(self.screen, (255, 255, 255), (x1 + grosor2, y1), (x2 + grosor2, y2),
+                                 branch_thickness_random)
+                grosor2 -= 1
 
             # Ramificaciones derecha
             i = random.randint(branch_quantity[0], branch_quantity[1])
             while i != 0:
                 fork_angle_random = random.randint(fork_angle[0], fork_angle[1])
-                self.drawTree(x2, y2, angle + (fork_angle_random * i), depth - 1, branch_thickness, fork_angle, base_len, branch_quantity)
+                self.drawTree(x2, y2, angle + (fork_angle_random * i), depth - 1, branch_thickness, fork_angle,
+                              base_len, branch_quantity, grosor - 2)
                 i -= 1
 
-            #ramificaciones Izquierda
+            # ramificaciones Izquierda
             i = random.randint(branch_quantity[0], branch_quantity[1])
             while i != 0:
                 fork_angle_random = random.randint(fork_angle[0], fork_angle[1])
-                self.drawTree(x2, y2, angle - (fork_angle_random * i), depth - 1, branch_thickness, fork_angle, base_len, branch_quantity)
+                self.drawTree(x2, y2, angle - (fork_angle_random * i), depth - 1, branch_thickness, fork_angle,
+                              base_len, branch_quantity, grosor - 2)
                 i -= 1
 
     def input(self, event):
