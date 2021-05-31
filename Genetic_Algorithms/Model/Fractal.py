a6387d049446de5caea3cb82374b7b5c55553f0a
Brayan Marín Quirós <brayanmq11288@gmail.com>
2021-05-31 15:31:31 -0600

    Algoritmo fractal modificado (errores ramas)

--- Genetic_Algorithms/Model/Fractal.py
+++ Genetic_Algorithms/Model/Fractal.py
@@ -17,12 +17,12 @@ class Tree:
         x1 = 300
         y1 = 550
         angle = -90
-        depth = 7
-        grosor = 8
+        depth = 6
+        grosor = 5
         branch_thickness = (1, 2)
         fork_angle = (20.0, 30.0)
-        branch_quantity = (1, 3)
-        base_len = (10.0, 15.0)
+        branch_quantity = (3, 3)
+        base_len = (12.0, 12.0)
 
         self.drawTree(x1, y1, angle, depth, branch_thickness, fork_angle, base_len, branch_quantity, grosor)
         pygame.display.flip()
@@ -48,21 +48,31 @@ class Tree:
                                  branch_thickness_random)
                 grosor2 -= 1
 
-            # Ramificaciones derecha
+            # Cantidad de ramas random
             i = random.randint(branch_quantity[0], branch_quantity[1])
-            while i != 0:
-                fork_angle_random = random.randint(fork_angle[0], fork_angle[1])
-                self.drawTree(x2, y2, angle + (fork_angle_random * i), depth - 1, branch_thickness, fork_angle,
-                              base_len, branch_quantity, grosor - 2)
-                i -= 1
 
-            # ramificaciones Izquierda
-            i = random.randint(branch_quantity[0], branch_quantity[1])
-            while i != 0:
-                fork_angle_random = random.randint(fork_angle[0], fork_angle[1])
-                self.drawTree(x2, y2, angle - (fork_angle_random * i), depth - 1, branch_thickness, fork_angle,
+            # Si la cantidad de ramas es impar
+            if not i % 2 == 0:
+                self.drawTree(x2, y2, angle, depth - 1, branch_thickness, fork_angle,
                               base_len, branch_quantity, grosor - 2)
-                i -= 1
+
+            if i != 1:
+                # Ramificaciones derecha
+                while i > 0:
+                    fork_angle_random = random.randint(fork_angle[0], fork_angle[1])
+                    self.drawTree(x2, y2, angle + (fork_angle_random * i), depth - 1, branch_thickness, fork_angle,
+                                  base_len, branch_quantity, grosor - 2)
+                    self.drawTree(x2, y2, angle - (fork_angle_random * i), depth - 1, branch_thickness, fork_angle,
+                                  base_len, branch_quantity, grosor - 2)
+                    i -= 1
+
+                # ramificaciones Izquierda
+                # i = random.randint(branch_quantity[0], branch_quantity[1])
+                # while i != 0:
+                #     fork_angle_random = random.randint(fork_angle[0], fork_angle[1])
+                #     self.drawTree(x2, y2, angle - (fork_angle_random * i), depth - 1, branch_thickness, fork_angle,
+                #                   base_len, branch_quantity, grosor - 2)
+                #     i -= 1
 
     def input(self, event):
         if event.type == pygame.QUIT:
